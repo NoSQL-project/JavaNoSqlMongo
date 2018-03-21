1be6570bd25b6e6768fe4567fbdcfb199b70a7e2
Robin <robin.neu@devinci.fr>
2018-03-21 15:41:41 +0100

    oui

---
+++ src/main/java/org/oui/dingue/MJR/App.java
@@ -0,0 +1,13 @@
+package org.oui.dingue.MJR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
